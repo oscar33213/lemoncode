2ece52ac7cb69ac9e9224855d1765297d16968ce
Oscar <oscar.h@gmail.com>
2024-02-29 13:38:25 +0100

    cambios-funcion

--- funciones.js
+++ funciones.js
@@ -37,3 +37,18 @@ botonEnviar.addEventListener("click", getSaludo)
 //CARRUSEL DE FOTOS Y FAT ARROW
 
 
+function cambiarFotoPrincipal(idMiniatura){
+    const fotoprincipal = document.getElementById("foto-principal");
+    const miniatura = document.getElementById(idMiniatura);
+
+    fotoprincipal.src = miniatura.src;
+}
+
+
+const miniatura1 = document.getElementById("miniatura1");
+const miniatura2 = document.getElementById("miniatura2");
+const miniatura3 = document.getElementById("miniatura3");
+
+miniatura1.addEventListener("click", function(){cambiarFotoPrincipal("miniatura1")})
+miniatura2.addEventListener("click", function(){cambiarFotoPrincipal("miniatura2")})
+miniatura3.addEventListener("click", function(){cambiarFotoPrincipal("miniatura3")})
